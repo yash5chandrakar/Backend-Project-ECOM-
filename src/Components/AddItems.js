0924589643de98ceb1a7a2cdde70d1064a8ce368
yash5chandrakar <yash5chandrakar@gmail.com>
2022-10-07 17:23:19 +0530

    final commit

--- src/Components/AddItems.js
+++ src/Components/AddItems.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import styles from "./AddItems.module.css"
 
 const AddItems = () => {
@@ -71,162 +72,61 @@ const AddItems = () => {
         else {
             alert("Error! Some Details were incorrect.")
         }
-<<<<<<< HEAD
     }
-
-    useEffect(() => {
-        if (params.id && params.id !== "") {
-            async function getItem() {
-                const res = await fetch(`/getSingleItem?id=${params.id}`, {
-                    method: "GET",
-                }).then((resp => resp.json()))
-                setImageUrl(res.image)
-                setName(res.name)
-                setPrice(res.price)
-                setBrand(res.brand)
-                setDesc(res.desc)
-                setWarranty(res.warranty)
-                // console.log(res.highlights)
-
-                if (res.highlights && res.highlights[0] !== "") {
-                    setHighlights1(res.highlights[0])
-                }
-                if (res.highlights && res.highlights[1] !== "") {
-                    setHighlights2(res.highlights[1])
-                }
-                if (res.highlights && res.highlights[2] !== "") {
-                    setHighlights3(res.highlights[2])
-                }
-                if (res.highlights && res.highlights[3] !== "") {
-                    setHighlights4(res.highlights[3])
-                }
-            }
-            getItem()
-        }
-    }, [params])
-
-
     return (
         <div className={styles.container}>
             <form onSubmit={(e) => handleSubmit(e)}>
                 <h1>Add an Item</h1><br />
                 <p>You can add and edit your details of your item price, description etc. </p><br />
                 <div className={styles.row}>
-                    <div >
+                    <div className={styles.col - 25}>
                         <label htmlFor='name' >Name : </label>
                     </div>
-                    <div >
-                        <input value={name} type={'text'} required onChange={(e) => setName(e.target.value)} placeholder="Enter Name of Product" /> <br />
+                    <div className={styles.col - 75}>
+                        <input value={name} type={'text'} required onChange={(e) => setName(e.target.value)} /> <br />
                     </div>
                 </div>
                 <div className={styles.row}>
-                    <div >
+                    <div className={styles.col - 25}>
                         <label htmlFor='price'>Price : </label>
                     </div>
-                    <div >
-                        <input value={price} type={'text'} required onChange={(e) => setPrice(e.target.value)} placeholder="Enter Price of Product" /> <br />
+                    <div className={styles.col - 75}>
+                        <input value={price} type={'text'} required onChange={(e) => setPrice(e.target.value)} /> <br />
                     </div>
                 </div>
                 <div className={styles.row}>
-                    <div >
+                    <div className={styles.col - 25}>
                         <label htmlFor='brand'>Brand Name : </label>
                     </div>
-                    <div >
-                        <input value={brand} type={'text'} required onChange={(e) => setBrand(e.target.value)} placeholder="Enter Brand Name" /> <br />
+                    <div className={styles.col - 75}>
+                        <input value={brand} type={'text'} required onChange={(e) => setBrand(e.target.value)} /> <br />
                     </div>
                 </div>
                 <div className={styles.row}>
-                    <div >
+                    <div className={styles.col - 25}>
                         <label htmlFor='highlights'>Highlights : </label>
                     </div>
-                    <div >
-                        <input value={highlights1} type={'text'} required onChange={(e) => setHighlights1(e.target.value)} placeholder="Enter Highlight 1" />
-                        <input value={highlights2} type={'text'} required onChange={(e) => setHighlights2(e.target.value)} placeholder="Enter Highlight 2" />
-                        <input value={highlights3} type={'text'} required onChange={(e) => setHighlights3(e.target.value)} placeholder="Enter Highlight 3" />
-                        <input value={highlights4} type={'text'} required onChange={(e) => setHighlights4(e.target.value)} placeholder="Enter Highlight 4" />
+                    <div className={styles.col - 80}>
+                        <input value={brand} type={'text'} required onChange={(e) => setBrand(e.target.value)} />
+                        <input value={brand} type={'text'} required onChange={(e) => setBrand(e.target.value)} /> <br />
+                        <input value={brand} type={'text'} required onChange={(e) => setBrand(e.target.value)} />
+                        <input value={brand} type={'text'} required onChange={(e) => setBrand(e.target.value)} /> <br />
                     </div>
                 </div>
                 <div className={styles.row}>
-                    <div >
-                        <label htmlFor='desc'>Description (Short) : </label>
+                    <div className={styles.col - 25}>
+                        <label htmlFor='desc'>Description (Short) : </label><br />
                     </div>
-                    <div >
-                        <textarea type={'text'} required onChange={(e) => setDesc(e.target.value)} value={desc} placeholder="Enter some description of Product..." /> <br />
+                    <div className={styles.col - 75}>
+                        <textarea type={'text'} required onChange={(e) => setDesc(e.target.value)} value={desc} /> <br />
                     </div>
                 </div>
                 <div className={styles.row}>
-                    <div >
+                    <div className={styles.col - 25}>
                         <label htmlFor='imageUrl'>Image Url: </label>
                     </div>
-                    <div >
-                        <input type={'text'} value={imageFile} onChange={(e) => setImageUrl(e.target.value)}
-                            placeholder="Enter Image URL" required />
-                    </div>
-                </div>
-                <div className={styles.row}>
-                    <div >
-                        <label htmlFor='imageUrl'>Warranty Period(in Years) : </label>
-                    </div>
-                    <div >
-                        <input id={styles.warranty} type={'number'} min="0" max="10" value={warranty} onChange={(e) => setWarranty(e.target.value)}
-                            placeholder="Enter Warranty Period" required />
-=======
-     }
-    return (
-        <div className={styles.container}>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <h1>Add an Item</h1><br/>
-                <p>You can add and edit your details of your item price, description etc. </p><br/>
-                <div className={styles.row}>
-                    <div className={styles.col-25}>
-                       <label htmlFor='name' >Name : </label>
-                    </div>
-                    <div className={styles.col-75}>
-                       <input  value={name} type={'text'} required onChange={(e) => setName(e.target.value)} /> <br />
-                    </div>
-                </div>
-                <div className={styles.row}>
-                   <div className={styles.col-25}>    
-                      <label htmlFor='price'>Price : </label>
-                    </div>
-                    <div className={styles.col-75}>
-                      <input value={price} type={'text'} required onChange={(e) => setPrice(e.target.value)} /> <br />
-                    </div>
-                </div>
-                <div className={styles.row}>
-                   <div className={styles.col-25}>  
-                      <label htmlFor='brand'>Brand Name : </label>
-                    </div>
-                    <div className={styles.col-75}>
-                      <input value={brand} type={'text'} required onChange={(e) => setBrand(e.target.value)} /> <br />
-                    </div>
-                </div>
-                <div className={styles.row}>
-                   <div className={styles.col-25}>  
-                      <label htmlFor='highlights'>Highlights : </label>
-                    </div>
-                    <div className={styles.col-80}>
-                      <input  value={brand} type={'text'} required onChange={(e) => setBrand(e.target.value)} /> 
-                      <input  value={brand} type={'text'} required onChange={(e) => setBrand(e.target.value)} /> <br />
-                      <input  value={brand} type={'text'} required onChange={(e) => setBrand(e.target.value)} /> 
-                      <input  value={brand} type={'text'} required onChange={(e) => setBrand(e.target.value)} /> <br />
-                    </div>
-                </div>
-                <div className={styles.row}>
-                   <div className={styles.col-25}>
-                      <label htmlFor='desc'>Description (Short) : </label><br /> 
-                   </div>
-                   <div className={styles.col-75}>
-                      <textarea  type={'text'} required onChange={(e) => setDesc(e.target.value)} value={desc} /> <br />
-                   </div>
-                </div>
-                <div className={styles.row}>
-                   <div className={styles.col-25}>
-                      <label htmlFor='imageUrl'>Image Url: </label> 
-                    </div>
-                    <div className={styles.col-75}>
-                      <input  type={'text'} onChange={(e) => setImageUrl(e.target.value)} required /> <br />
->>>>>>> a8c62fb87bf3395bbd1c422320dea23630cdac31
+                    <div className={styles.col - 75}>
+                        <input type={'text'} onChange={(e) => setImageUrl(e.target.value)} required /> <br />
                     </div>
                 </div>
                 <div className={styles.submitBtn}>
@@ -238,91 +138,3 @@ const AddItems = () => {
 }
 
 export default AddItems;
-<<<<<<< HEAD
-
-// import React, { useEffect, useState } from 'react'
-// import { useParams } from 'react-router-dom'
-// import styles from "./AddItems.module.css"
-
-// const AddItems = () => {
-//     const params = useParams()
-//     // console.log(params)
-
-//     useEffect(() => {
-//         if (params.id && params.id !== "") {
-//             async function getItem() {
-//                 const res = await fetch(`/getSingleItem?id=${params.id}`, {
-//                     method: "GET",
-//                 }).then((resp => resp.json()))
-
-//                 setName(res.name)
-//                 setPrice(res.price)
-//                 setBrand(res.brand)
-//                 setDesc(res.desc)
-//                 setImageUrl(res.image)
-//             }
-//             getItem()
-//         }
-//     }, [params])
-
-
-//     const [name, setName] = useState("")
-//     const [price, setPrice] = useState("")
-//     const [brand, setBrand] = useState("")
-//     const [desc, setDesc] = useState("")
-//     const [imageFile, setImageUrl] = useState("")
-
-//     async function handleSubmit(e) {
-//         e.preventDefault()
-
-//         const myData = {
-//             name: name,
-//             price: price,
-//             brand: brand,
-//             desc: desc,
-//             image: imageFile
-//         }
-
-//         const res = await fetch("/addItems", {
-//             method: "POST",
-//             headers: {
-//                 "Content-type": "application/json"
-//             },
-//             body: JSON.stringify(myData)
-//         })
-
-
-//         if (res.ok) {
-//             alert("Data Added Successfully")
-//             setBrand("")
-//             setDesc("")
-//             setName("")
-//             setPrice("")
-//             setImageUrl("")
-//         }
-//         else {
-//             alert("Error! Some Details were incorrect.")
-//         }
-//     }
-//     return (
-//         <div className={styles.outerDiv}>
-//             <form onSubmit={(e) => handleSubmit(e)}>
-//                 <h1>Add an Item</h1>
-//                 <div>
-//                     <label htmlFor='name'>Name : </label><input value={name} type={'text'} required onChange={(e) => setName(e.target.value)} /> <br />
-//                     <label htmlFor='price'>Price : </label><input value={price} type={'number'} required onChange={(e) => setPrice(e.target.value)} /> <br />
-//                     <label htmlFor='brand'>Brand Name : </label><input value={brand} type={'text'} required onChange={(e) => setBrand(e.target.value)} /> <br />
-//                     <label htmlFor='desc'>Description (Short) : </label><br /> <textarea type={'text'} required onChange={(e) => setDesc(e.target.value)} value={desc} /> <br />
-//                     <label htmlFor='imageUrl'>Image Url: </label> <input type={'text'} onChange={(e) => setImageUrl(e.target.value)} required /> <br />
-//                     <input type={'submit'} value="Submit" required />
-//                 </div>
-//             </form>
-//         </div>
-//     )
-// }
-
-// export default AddItems
-
-// More things to include :- tags[], highlights,ratings
-=======
->>>>>>> a8c62fb87bf3395bbd1c422320dea23630cdac31
